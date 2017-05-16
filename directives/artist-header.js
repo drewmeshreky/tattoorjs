7e3943a6749b2242e307fe20455ac6072f15a48c
Drew Meshreky <drew@tattoor.co>
2017-05-15 21:35:39 -0700

    changed the model in the header controller

--- directives/artist-header.js
+++ directives/artist-header.js
@@ -4,9 +4,11 @@ angular.module('artist-header', [])
 		restrict: 'E',
       	templateUrl: 'templates/artist-header.html',
 	  	controller: function () {
-			this.name = 'Tattoor';
-			this.description = 'Online appointment requests built for tattoo artists';
-			this.image = 'https://scontent-dft4-3.cdninstagram.com/t51.2885-19/s320x320/18300017_424845637895118_1725354621263675392_a.jpg';
+			this.artist = {
+				name: 'Tattoor',
+				description: 'Online appointment requests built for tattoo artists',
+				image: 'https://scontent-dft4-3.cdninstagram.com/t51.2885-19/s320x320/18300017_424845637895118_1725354621263675392_a.jpg'
+			};
 		
 			$(".header").sticky();
 	  	},
