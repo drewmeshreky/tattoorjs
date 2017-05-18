1f927639629f2d83051ca5662bc91f4833208668
Drew Meshreky <drew@tattoor.co>
2017-05-17 21:36:26 -0700

    Starting to layout reference photos

--- directives/artist-contact.js
+++ directives/artist-contact.js
@@ -9,6 +9,8 @@ angular.module('artist-contact', [])
 				{ question: "How do you make money?", answer: "Right now, that's not our main focus. Our main focus is building a tool that artists love. If we do that, we know we'll find a way to build a sustainable business." },
 				{ question: "How do I get started?", answer: "Talk to me! I can get you set up."},
 			];
+			
+			this.referencePhotos = [];
 		},
 		controllerAs: 'artistContact'
 	};
