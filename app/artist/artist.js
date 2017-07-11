63f965a619cf0963f6bceb067f841d923ca20083
Drew Meshreky <drew@tattoor.co>
2017-07-10 23:00:10 -0700

    Scope apply for image uploads

--- app/artist/artist.js
+++ app/artist/artist.js
@@ -1,6 +1,6 @@
 var module = angular.module('tattoor');
 
-var ArtistController = function($http) {
+var ArtistController = function($http, $scope) {
 
 	this.requestStates = {
 	    UNDEFINED: 0,
@@ -14,15 +14,16 @@ var ArtistController = function($http) {
 			description: 'This is a demo of Tattoor, the online appointment app built for tattoo artists. Fill out the form to see how it works.',
 		};		
 
+		this.request = {};
+		this.request.images = [];
+
 		this.requestState = this.requestStates.UNDEFINED;
 		this.reponseMessage = '';
 
 		this.placementOptions = ["Open to Suggestion","Head","Neck","Chest","Back","Shoulder","Upper Arm","Forearm","Stomach","Thigh","Calf","Other"];
 		this.styleOptions = ["Black & Gray","Color","Open to Suggestion"];
 		this.budgetOptions = ["$100","$100-$250","$250-$500","$500-$1000","$1000-$2000","$2000-$5000","$5000+"];
-		
-		this.referenceImages = [];
-		
+				
 		this.fsClient = filestack.init('A77HiFpIdSIuKv3ihmKw7z');
 	};
 		
@@ -36,20 +37,18 @@ var ArtistController = function($http) {
 		.then(
 			function(result) {
 				result.filesUploaded.forEach(function(fileUploaded) {
-					console.log(JSON.stringify(fileUploaded));
-					console.log(fileUploaded.url);
-					self.referenceImages.push(fileUploaded);
+					self.request.images.push(fileUploaded);
+					$scope.$apply();
 				})			
 			}
 		);
 	}
 
-	this.submitRequest = function(request) {
+	this.submitRequest = function() {
 		var self = this;
-		request.referenceImages = self.referenceImages;
 		
 		$http.post(
-			"https://tattoor.azurewebsites.net/api/ApptRequest?code=UBg8xPsHOhFccQhQMjrOKuTPLQWRPzFimeaQ5H6JMt8wwOryUZrh4w==", request)
+			"https://tattoor.azurewebsites.net/api/ApptRequest?code=UBg8xPsHOhFccQhQMjrOKuTPLQWRPzFimeaQ5H6JMt8wwOryUZrh4w==", self.request)
 		.then(
 			function(response) {
 				self.requestState = self.requestStates.SENT;
